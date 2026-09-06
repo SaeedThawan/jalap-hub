2a7ee9cdfd19a30cf1a341a8da195941e4417624
SaeedThawan <saeedthawan96@gmail.com>
2026-09-06 10:06:00 +0300

    Format and clean up config.js file

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   APP_NAME: "بوابة جلب | Jalap Hub",
   LOGO_PATH: "assets/logo.png",
   
-  // رابط Google Apps Script الخاص بك (تأكد أنه آخر رابط نشر Web App)
+  // رابط Google Apps Script الخاص بك 
   API_URL: "https://script.google.com/macros/s/AKfycbw1DTvmz2e-eoKP-FxRD0I-742FwPFi6Fk3ag201fw4QWC3Sodq63R4O07fGY92T7Yi/exec",
 
   DEFAULT_GENERAL_RULES: {
