9431b17c8e762bb0d5602c9a1ca3a0aace75f450
SaeedThawan <saeedthawan96@gmail.com>
2026-09-06 17:09:21 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 /**
- * إعدادات منظومة جلب العالمية - الإصدار 42.0
+ * إعدادات منظومة جلب العالمية - Config v43.0
  */
 const CONFIG = {
   COMPANY_NAME: 'شركة جلب العالمية للتجارة',
   LOGO_PATH: 'assets/logo.png',
   
-  // الرابط البرمجي لنشر الويب في Google Apps Script
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyf2pX-q3M9pLp8iWbU1Hw6w1vT_o9I5gB2tq_V1X4/exec',
+  // تأكد أن الرابط ينتهي بـ /exec
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyc2pX-q3M9pLp8iWbU1Hw6w1vT_o9I5gB2tq_V1X4/exec',
 
   DEFAULT_GENERAL_RULES: {
     generalThresholdPct: 80,
