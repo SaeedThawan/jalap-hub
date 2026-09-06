28869090f2d2180716bb13f6205c7b9a3a9f9eb4
SaeedThawan <saeedthawan96@gmail.com>
2026-09-06 09:24:10 +0300

    Update CONFIG settings to version 36.0

--- js/config.js
+++ js/config.js
@@ -1,12 +1,12 @@
 /**
- * إعدادات منظومة جلب العالمية للتجارة v2.0
+ * إعدادات منظومة جلب العالمية للتجارة v36.0
  */
 const CONFIG = {
   COMPANY_NAME: "شركة جلب العالمية للتجارة",
   APP_NAME: "بوابة جلب | Jalap Hub",
   LOGO_PATH: "assets/logo.png",
   
-  // رابط Google Apps Script الخاص بك
+  // رابط Google Apps Script الخاص بك (تأكد أنه آخر رابط نشر Web App)
   API_URL: "https://script.google.com/macros/s/AKfycbw1DTvmz2e-eoKP-FxRD0I-742FwPFi6Fk3ag201fw4QWC3Sodq63R4O07fGY92T7Yi/exec",
 
   DEFAULT_GENERAL_RULES: {
